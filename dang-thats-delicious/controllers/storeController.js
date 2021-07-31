94eb05ee0ff26cfe13e8a5fa19bee5432e387c0b
Robert Schulte <schulte.robertw@gmail.com>
2021-07-31 15:58:01 -0500

    added location to Store model

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -12,7 +12,7 @@ exports.addStore = (req, res) => {
 exports.createStore = async (req, res) => {
     const store = await (new Store(req.body)).save()
     req.flash('success', `Successfully created ${store.name}. Care to leave a review?`)
-    res.redirect(`/stores/${store.slug}`)
+    res.redirect(`/store/${store.slug}`)
 }
 
 exports.getStores = async (req, res) => {
@@ -36,7 +36,7 @@ exports.updateStore = async (req, res) => {
         new: true, // return the new store instead of the old one
         runValidators: true
     }).exec()
-    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`)
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/store/${store.slug}">View Store</a>`)
     // Redirect them to the store and tell them it worked
     res.redirect(`/stores/${store._id}/edit`)
 }
